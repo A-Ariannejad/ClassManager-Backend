c2868f27dbb53cc29d1d6ba7495a025e5787d91c
A_Ariannejad <ariannejad14@gmail.com>
2023-09-28 18:40:32 +0330

    the show and the update is added

--- ClassManager/ClassManager/urls.py
+++ ClassManager/ClassManager/urls.py
@@ -21,4 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', include('CustomUsers.urls')),
+    path('class/', include('CustomClasses.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
