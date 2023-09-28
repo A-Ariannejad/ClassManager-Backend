05ba99cfc4176a960934a1cdc3a210351745b14b
A_Ariannejad <ariannejad14@gmail.com>
2023-09-28 14:14:01 +0330

    the custom class is added

--- ClassManager/CustomClasses/models.py
+++ ClassManager/CustomClasses/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from CustomUsers.models import CustomUser
 
-# Create your models here.
+class CustomClass(models.Model):
+    name = models.TextField(max_length=50)
+    teacher = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    CATEGORIES = (
+        ('other', 'Other'),
+        ('cloth', 'Cloth'),
+        ('laptop', 'Laptop'),
+        ('mobile', 'Mobile'),
+        ('electronic', 'Electronic'),
+        ('furniture', 'furniture')
+    )
+    category = models.CharField(max_length=20, choices=CATEGORIES, default='other')
+    description = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
