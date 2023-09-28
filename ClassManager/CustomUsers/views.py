f2d580b8cb94dc18d045da36b7a6d50ae4bbc4fd
A_Ariannejad <ariannejad14@gmail.com>
2023-09-28 15:14:21 +0330

    show use is added

--- ClassManager/CustomUsers/views.py
+++ ClassManager/CustomUsers/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions, viewsets, status
+from rest_framework.response import Response
 from .models import CustomUser
 from .serializers import CustomUserSerializer, LoginSignupCustomUserSerializer
 from django.contrib.auth.hashers import make_password, check_password
@@ -17,3 +18,22 @@ class CreateCustomUser(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(password = make_password(self.request.data.get('password')))
+
+
+class MyCustomUser(generics.RetrieveAPIView):
+    serializer_class = CustomUserSerializer
+    # permission_classes = [permissions.AllowAny]
+    def get(self, request, *args, **kwargs):
+        try:
+            user_id = request.user.id
+            user = CustomUser.objects.get(id=user_id)
+            serializer = self.serializer_class(user)
+            return Response(serializer.data)
+        except CustomUser.DoesNotExist:
+            return Response({'error': 'User does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+        
+class ShowCustomUser(generics.RetrieveAPIView):
+    # permission_classes = [IsEditUser]
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    lookup_field = 'id'
